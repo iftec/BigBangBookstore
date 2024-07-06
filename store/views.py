d9244ee8c526dd719fbffa5429ebeb734668dc58
iftec <iftec@users.noreply.github.com>
2024-07-06 12:35:19 +0100

    Flesh out Products Template
    Add more style to the products template, including adding background header image.
    Use bootstrap cards to set layout for books list.
    Change products view to list view.

--- store/views.py
+++ store/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
+from .models import Product
 
 
-class StoreFront(generic.TemplateView):
+class StoreFront(generic.ListView):
     template_name = "storefront.html"
+    queryset = Product.objects.all()
+    context_object_name = "products"
