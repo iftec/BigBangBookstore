db84ef05fa12f9705751534f2daf594d801498a1
iftec <iftec@users.noreply.github.com>
2024-07-09 11:56:34 +0100

    Search function added
    Add functionality to search on the store front template. User can now search by book title or author.

--- store/views.py
+++ store/views.py
@@ -1,16 +1,27 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views import generic
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.views import LoginView
 from .forms import UserLoginForm
+from django.db.models import Q
 
 
 class StoreFront(generic.ListView):
     template_name = "storefront.html"
-    queryset = Product.objects.all().order_by('name')
     context_object_name = "products"
     paginate_by = 12
+
+    # Define search function
+    def get_queryset(self):
+        queryset = Product.objects.all().order_by('name')
+        search_query = self.request.GET.get('search', '')
+        if search_query:
+            queryset = queryset.filter(Q(name__icontains=search_query) |
+                                       Q(author__icontains=search_query))
+        return queryset
 
 
 class ProductDetails(generic.DetailView):
