4732944a355e5db03dc34b6018f0034090436f5a
iftec <iftec@users.noreply.github.com>
2024-07-12 10:11:34 +0100

    Update basket app
    Connect the basket to user accounts, so now if a user is logged in it will save the basket for that user.
    Update add and remove from basket functions to allow for user-stored baskets.

--- basket/urls.py
+++ basket/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.BasketView.as_view(), name='basket'),
     path('add/<int:product_id>/', views.AddToBasketView.as_view(),
          name='add-to-basket'),
-    path('remove/<int:product_id>/', views.RemoveFromBasketView.as_view(),
+    path('remove/<int:item_id>/', views.RemoveFromBasketView.as_view(),
          name='remove-from-basket'),
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,25 +1,50 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic import ListView, View
 from store.models import Product
+from .models import Basket, BasketItem
 from .forms import AddToBasketForm
+from django.contrib import messages
 
 
 class BasketView(ListView):
     def get(self, request, *args, **kwargs):
-        # Get the sessions basket, will create if dosen't exisit
-        basket = request.session.get('basket', {})
-        # Create an items dictionary
-        items = []
-        total_price = 0
-        # For any items already in the items dictionary
-        # loop through and get from database
-        for product_id, quantity, in basket.items():
-            product = get_object_or_404(Product, id=product_id)
-            items.append({'product': product, 'quantity': quantity,
-                          'total_price': quantity * product.price})
-            total_price += quantity * total_price
-        return render(request, 'basket.html',
-                      {'items': items, 'total_price': total_price})
+        if request.user.is_authenticated:
+            # Get users basket or create a new one if dosen't exist
+            # and flag if it's a new basket
+            basket, new_basket = Basket.objects.get_or_create(user=request.user)
+            basket_items = BasketItem.objects.filter(basket=basket)
+            # Define variables
+            items = []
+            total_price = 0
+            # Loop through any items in the basket
+            # and get product info from database
+            for item in basket_items:
+                product = get_object_or_404(Product, id=item.product.id)
+                line_cost = item.quantity * product.price
+                # Add each item to the items list
+                items.append({'product': product, 'quantity': item.quantity,
+                              'line_cost': line_cost, 'id': item.id})
+                # Update the total basket price
+                total_price += total_price + line_cost
+            return render(request, 'basket.html',
+                          {'items': items, 'total_price': total_price})
+        else:
+            # Get or create a session basket
+            basket = request.session.get('basket', {})
+            # Create an items list & set initial basket cost
+            items = []
+            total_price = 0
+            # Loop through any items in the basket
+            # and get the product info from the database
+            for product_id, quantity, in basket.items():
+                product = get_object_or_404(Product, id=product_id)
+                # Add each item to the items list
+                items.append({'product': product, 'quantity': quantity,
+                              'line_cost': quantity * product.price})
+                # Update the basket cost
+                total_price += quantity * total_price
+            return render(request, 'basket.html',
+                          {'items': items, 'total_price': total_price})
 
 
 class AddToBasketView(View):
@@ -28,28 +53,70 @@ class AddToBasketView(View):
         product = get_object_or_404(Product, id=kwargs['product_id'])
         form = AddToBasketForm(request.POST)
         if form.is_valid():
-            # Get the session's basket
-            basket = request.session.get('basket', {})
             product_id = str(product.id)
-            # If item isn't in basket already, add it to the basket,
-            # otherwise add to the existing quantity
-            if product_id in basket:
-                basket[product_id] += form.cleaned_data['quantity']
+            if request.user.is_authenticated:
+                # Get users basket or create one if dosen't exist
+                # seperate returned tuple
+                user_basket, new_basket = Basket.objects.get_or_create(
+                    user=request.user)
+                # Check for the same exisiting items basket
+                # or flag if it's a new item
+                basket_item, new_item = BasketItem.objects.get_or_create(
+                    basket=user_basket, product=product)
+                if new_item:
+                    # Add the requested quantity to the basket
+                    basket_item.quantity = form.cleaned_data['quantity']
+                    print(form.cleaned_data['quantity'])
+                else:
+                    # Or increase by the requested quantity
+                    basket_item.quantity += form.cleaned_data['quantity']
+                    print(form.cleaned_data['quantity'])
+                # Save the updated item
+                basket_item.save()
             else:
-                basket[product_id] = form.cleaned_data['quantity']
-            request.session['basket'] = basket
+                # Get the session's basket
+                basket = request.session.get('basket', {})
+                # If item isn't in basket already, add it to the basket,
+                # otherwise add to the existing quantity
+                if product_id in basket:
+                    basket[product_id] += form.cleaned_data['quantity']
+                else:
+                    basket[product_id] = form.cleaned_data['quantity']
+                request.session['basket'] = basket
         return redirect('basket:basket')
 
 
 class RemoveFromBasketView(View):
     def post(self, request, *args, **kwargs):
-        # Get the session's basket
-        basket = request.session.get('basket', {})
-        # Get the selected products id
-        product_id = str(kwargs['product_id'])
-        # Delete it from the basket
-        if product_id in basket:
-            del basket[product_id]
-        # Update the session's basket
-        request.session['basket'] = basket
+        if request.user.is_authenticated:
+            # Get the item from the passed id and delete from basket
+            item = get_object_or_404(BasketItem, id=kwargs['item_id'],
+                                     basket__user=request.user)
+            # Shorten book name for messages
+            if len(item.product.name) > 30:
+                item_name = item.product.name[:30] + '...'
+            else:
+                item_name = item.product.name
+
+            try:
+                item.delete()
+                messages.success(
+                    request, f'"{item_name}" was removed from your basket.')
+            except BasketItem.DoesNotExist:
+                messages.error(
+                    request, f'"{item_name}" is not in your basket.')
+        else:
+            # Get the basket from the session data
+            basket = request.session.get('basket', {})
+            # Get the id and check if it exists and delete
+            product_id = str(kwargs['id'])
+            if product_id in basket:
+                del basket[product_id]
+                messages.success(
+                    request, f'Book "{item_name}" removed from your basket.')
+            else:
+                messages.error(
+                    request, f'Book "{item_name}" is not in your basket.')
+            # Update the session basket
+            request.session['basket'] = basket
         return redirect('basket:basket')
